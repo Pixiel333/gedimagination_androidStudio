d5daf2f8e12f5126d160e6c2b7978645e3971ca3
matis-s-se <matissedimier@gmail.com>
2021-03-09 12:43:01 +0100

    Ajout Liste photo

---
+++ app/src/main/java/bts/sio/gedimaginationandroid/ListePhotos.java
@@ -0,0 +1,14 @@
+package bts.sio.gedimaginationandroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ListePhotos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_liste_photos);
+    }
+}
