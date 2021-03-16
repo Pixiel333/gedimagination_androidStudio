aaaeffe15957f89183bee1c06795208d5e25672c
matis-s-se <matissedimier@gmail.com>
2021-03-16 11:52:19 +0100

    Creation page notation

---
+++ app/src/main/java/bts/sio/gedimaginationandroid/Notation.java
@@ -0,0 +1,14 @@
+package bts.sio.gedimaginationandroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Notation extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notation);
+    }
+}
